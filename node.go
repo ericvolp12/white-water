f359581d65d2f953ab5ebd141f20e01154f03121
Eric Volpert <evolpert@uchicago.edu>
2018-05-14 23:21:40 -0500

    ZeroMQ Logic abstracted into lib

--- node.go
+++ node.go
@@ -1,25 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
-	"fmt"
 
-	zmq "github.com/pebbe/zmq4"
+	"./zeromq"
 )
 
 type arrayFlags []string
-
-// Message is a Chistributed message struct
-type Message struct {
-	Type        string   `json:"type"`
-	ID          int      `json:"id"`
-	Destination []string `json:"destination"`
-	Key         string   `json:"key"`
-	Error       string   `json:"error"`
-	Source      string   `json:"source"`
-	Value       string   `json:"value"`
-}
 
 func (i *arrayFlags) String() string {
 	return "It's an array of peers"
@@ -43,63 +30,8 @@ func main() {
 
 	flag.Parse()
 
-	requester, err := zmq.NewSocket(zmq.REQ)
-	if err != nil {
-		fmt.Printf("Error creating requester: %v\n", err)
-	}
-	defer requester.Close()
-	fmt.Println("Trying to connect to router...")
-	requester.Connect(*routerEndpoint)
+	client := zeromq.CreateClient(*pubEndpoint, *routerEndpoint, *nodeName, peers)
 
-	err = requester.SetIdentity(*nodeName)
-	if err != nil {
-		fmt.Printf("Error identifying as: %v, %v\n", *nodeName, err)
-	}
+	zeromq.ReceiveMessages(&client)
 
-	sub, err := zmq.NewSocket(zmq.SUB)
-	if err != nil {
-		fmt.Printf("Error creating subscriber: %v\n", err)
-	}
-	defer sub.Close()
-	fmt.Println("Trying to connect to Pub Endpoint...")
-	sub.Connect(*pubEndpoint)
-
-	topic := *nodeName + " "
-
-	err = sub.SetLinger(0)
-	if err != nil {
-		fmt.Printf("Error lingering: %v\n", err)
-	}
-
-	err = sub.SetSubscribe(*nodeName)
-	if err != nil {
-		fmt.Printf("Error subscribing to: %v, %v\n", topic, err)
-	}
-
-	for {
-		msg, err := sub.RecvMessage(0)
-		if err != nil {
-			break
-		}
-		fmt.Println("Message received:")
-
-		cMsg := &Message{}
-		// Unwrap the message from JSON to Go
-		// Index 2 should be the json payload
-		json.Unmarshal([]byte(msg[2]), cMsg)
-		// Print the type of the message
-		fmt.Printf("\tType: %v\n", cMsg.Type)
-		fmt.Printf("\tFull message: %v\n", msg[2])
-		if cMsg.Type == "hello" {
-			reply, err := json.Marshal(Message{Type: "helloResponse", Source: *nodeName})
-			if err != nil {
-				break
-			}
-			sent, err := requester.SendMessage(reply)
-			if sent <= 0 || err != nil {
-				break
-			}
-
-		}
-	}
 }
---
+++ zeromq/zeromq.go
@@ -0,0 +1,121 @@
+package zeromq
+
+import (
+	"encoding/json"
+	"fmt"
+
+	zmq "github.com/pebbe/zmq4"
+)
+
+// Message is a Chistributed message struct
+type Message struct {
+	Type        string   `json:"type"`
+	ID          int      `json:"id"`
+	Destination []string `json:"destination"`
+	Key         string   `json:"key"`
+	Error       string   `json:"error"`
+	Source      string   `json:"source"`
+	Value       string   `json:"value"`
+}
+
+// Client defines a messenger client, they can send or receive messages
+type Client struct {
+	req      *zmq.Socket // Requester socket for the client
+	sub      *zmq.Socket // Subscriber socket for the client
+	nodeName string      // Name of this node
+	peers    []string    // Names of peers of this node
+}
+
+// CreateClient defines a client based on input information
+func CreateClient(pubEndpoint string, routerEndpoint string, nodeName string, peers []string) Client {
+
+	// Initialize Requester
+	requester, err := zmq.NewSocket(zmq.REQ)
+	if err != nil {
+		fmt.Printf("Error creating requester: %v\n", err)
+	}
+	fmt.Println("Requester client to connect to router...")
+	// Connect requester to router
+	requester.Connect(routerEndpoint)
+	// Set identity to our node name so we can get messages
+	err = requester.SetIdentity(nodeName)
+	if err != nil {
+		fmt.Printf("Error identifying as: %v, %v\n", nodeName, err)
+	}
+	// Initialize Subscriber
+	sub, err := zmq.NewSocket(zmq.SUB)
+	if err != nil {
+		fmt.Printf("Error creating subscriber: %v\n", err)
+	}
+	fmt.Println("Subscriber client trying to connect to Pub Endpoint...")
+	// Connect subscriber to publisher endpoint
+	sub.Connect(pubEndpoint)
+	// Linger set to 0 so we don't deadlock
+	err = sub.SetLinger(0)
+	if err != nil {
+		fmt.Printf("Error lingering: %v\n", err)
+	}
+	// Set subscription topic to our node name
+	err = sub.SetSubscribe(nodeName)
+	if err != nil {
+		fmt.Printf("Error subscribing to: %v, %v\n", nodeName, err)
+	}
+	// Create our client object
+	client := Client{req: requester, sub: sub, nodeName: nodeName, peers: peers}
+
+	// Send it back
+	return client
+}
+
+// DeleteClient closes open connections and deletes our client
+func DeleteClient(c *Client) {
+	// Close the requester
+	c.req.Close()
+	// Close the subscriber
+	c.sub.Close()
+}
+
+// ReceiveMessages starts a duty loop to handle ZeroMQ Messages on our subscriber
+func ReceiveMessages(c *Client) {
+	for {
+		msg, err := c.sub.RecvMessage(0)
+		if err != nil {
+			break
+		}
+		fmt.Println("Message received:")
+
+		cMsg := &Message{}
+		// Unwrap the message from JSON to Go
+		// Index 2 should be the json payload
+		json.Unmarshal([]byte(msg[2]), cMsg)
+		// Print the type of the message
+		fmt.Printf("\tType: %v\n", cMsg.Type)
+		fmt.Printf("\tFull message: %v\n", msg[2])
+		if cMsg.Type == "hello" {
+			// Send a hello response
+			err = SendMessage(c, Message{Type: "helloResponse", Source: c.nodeName})
+			if err != nil {
+				break
+			}
+
+		}
+	}
+}
+
+// SendMessage sends a message on a client's requester
+func SendMessage(c *Client, msg Message) error {
+	// Marshal message object into json
+	formattedMsg, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	// Send message via requester
+	sent, err := c.req.SendMessage(formattedMsg)
+	if err != nil {
+		return fmt.Errorf("Error sending message: %v", err)
+	}
+	if sent <= 0 {
+		return fmt.Errorf("Error sending message: zero bytes sent")
+	}
+	return nil
+}
