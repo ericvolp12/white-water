0cf41ec4ed2eba8e57f4537ea410de6071e7fe7b
Joseph <joseph@dontcallmejoe.com>
2018-05-14 23:58:33 -0500

    Starting to define some data structures for raft.

---
+++ raft/structures.go
@@ -0,0 +1,44 @@
+package raft
+
+type operation int
+
+const (
+	getOp operation = iota
+	setOp
+	deleteOp
+)
+
+type entry struct {
+	op    operation
+	key   string
+	value string
+}
+
+type leaderState struct {
+	nextIndex  []uint
+	matchIndex []uint
+}
+
+type volatileState struct {
+	commitIndex uint
+	lastApplied uint
+}
+
+type Sailor struct {
+	// Contains filtered or unexported fields
+	log         []entry
+	Table       map[string]string
+	currentTerm uint
+	votedFor    int
+	volatile    *volatileState
+	leader      *leaderState
+}
+
+type appendMessage struct {
+	term         uint
+	leaderId     int
+	prevLogIndex uint
+	prevLogTerm  uint
+	entries      []entry
+	leaderCommit uint
+}
