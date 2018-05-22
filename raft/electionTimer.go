97cace084e6c4ae013d061ba4c94cf1e03317886
Max Demers <mgdemers@uchicago.edu>
2018-05-22 17:49:04 -0500

    basic election timer

---
+++ raft/electionTimer.go
@@ -0,0 +1,22 @@
+package raft
+
+import(
+    "fmt"
+    "time"
+    "math/rand"
+)
+
+func (s *Sailor) timer(TIMEOUT_SIGNAL *chan bool) {
+    timeout := time.Duration((rand.Intn(150) + 150)) * time.Millisecond
+    last := s.lastMessageTime
+    for {
+        if (last != s.lastMessageTime) {
+            timeout = time.Duration((rand.Intn(150) + 150)) * time.Millisecond
+        }
+        last = s.lastMessageTime
+        if(time.Since(s.lastMessageTime) > timeout) {
+            fmt.Printf("TIMEOUT")
+            TIMEOUT_SIGNAL <- true // Sending TIMEOUT signal to raft thread
+        }
+    }
+}
