0c33ac0884cffd38b835ddd242ade46003a4266f
Joseph Marques <me@josephmarqu.es>
2018-05-22 17:48:40 -0500

    Comments

--- raft/raft.go
+++ raft/raft.go
@@ -2,16 +2,21 @@ package raft
 
 func (s *Sailor) MsgHandler(gets, sets, requestVote, appendEntry *chan messages.Message, timeouts *chan bool) {
 	select {
-	case msg := <-gets:
-		//get message handle
-	case msg := <-sets:
-		//set message handler
-	case msg := <-requestVote:
-		//requestVote message handle
-	case msg := <-appendEntry:
-		//append message handler
 	case msg := <-timeouts:
 		//timeouts message handle
+		//Max
+	case msg := <-gets:
+		//get message handle
+		//Joseph
+	case msg := <-sets:
+		//set message handler
+		//Joseph
+	case msg := <-requestVote:
+		//requestVote message handle
+		//Max
+	case msg := <-appendEntry:
+		//append message handler
+		//Max
 
 	}
 }
