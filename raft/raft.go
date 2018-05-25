998327bc219755070b3786226ed474fdfd988b04
Joseph Marques <me@josephmarqu.es>
2018-05-25 15:11:54 -0500

    MORE selects

--- raft/raft.go
+++ raft/raft.go
@@ -1,39 +1,50 @@
 package raft
 
 import (
-    "fmt"
-    "time"
-    "encoding/json"
-    "encoding/base64"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"time"
 )
 
-func (s *Sailor) MsgHandler(gets, sets, requestVote, appendEntry *chan messages.Message, timeouts *chan bool) {
+func (s *Sailor) MsgHandler(gets, sets, requestVote, appendEntry chan messages.Message, timeouts chan bool) {
 	for {
 		select {
-		case msg := <-*timeouts:
+		case msg := <-timeouts:
 			//timeouts message handle
 			//Max
 		default:
 			select {
 			case s.state == follower:
 				select {
-				//sets - J
-				//gets - J
-				//append - J
-				//requestVote - M
+				case msg := <-gets:
+					//Gets handle - Joseph
+				case msg := <-sets:
+					//Sets handle - Joseph
+				case msg := <-appendEntry:
+					//Append handle - Joseph
+				case msg := <-requestVote:
+					//Vote handle - Max
 				}
 			case s.state == candidate:
 				select {
-				//requestVote - M
-				//append -J
+				case msg := <-appendEntry:
+					//Append handle - Joseph
+				case msg := <-requestVote:
+					//VoteReply handle - Max
 				}
 			case s.state == leader:
 				select {
-				//gets -J
-				//sets -J
-				//append -J
-				//requestVote -M
-				//Propose-value?
+				case msg := <-gets:
+					//Gets handle - Joseph
+				case msg := <-sets:
+					//Sets handle - Joseph
+				case msg := <-appendEntry:
+					//AppendReply handle - Joseph
+				case msg := <-requestVote:
+					//Vote/VoteReply handle - Max
+
+					//Propose-value?
 				}
 			}
 		}
@@ -42,33 +53,32 @@ func (s *Sailor) MsgHandler(gets, sets, requestVote, appendEntry *chan messages.
 
 // Encodes different Raft message RPC structs into strings for ZMQ type messages
 func makePayload(payload interface{}) string {
-    temp, err := json.Marshal(payload)
-    if err != nil {
-        fmt.Printf("RequestVote RPC ERROR in handle_timeout\n")
-    } else {
-        return base64.StdEncoding.EncodeToString(temp)
-    }
+	temp, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("RequestVote RPC ERROR in handle_timeout\n")
+	} else {
+		return base64.StdEncoding.EncodeToString(temp)
+	}
 }
 
-
 func (s *Sailor) handle_timeout() {
-    // ONLY BECOME CANDIDATE IF ALLOWED
-    s.state = candidate
-    s.currentTerm += 1
-    s.numVotes = 1 // Votes for itself
-    s.lastMessageTime = time.Now() // Triggers timer thread to restart timer
+	// ONLY BECOME CANDIDATE IF ALLOWED
+	s.state = candidate
+	s.currentTerm += 1
+	s.numVotes = 1                 // Votes for itself
+	s.lastMessageTime = time.Now() // Triggers timer thread to restart timer
 
-    // Fill RequestVotes RPC struct
-    newmsg := make(requestVote)
-    newmsg.Term = s.currentTerm
-    newmsg.CandidateId = 0 // Current Node ID
-    newmsg.LastLogIndex = len(s.log) - 1    // Index of last entry in log
-    newmsg.LastLogTerm = s.log[newmsg.lastLogIndex].term    // The term of that entry index
+	// Fill RequestVotes RPC struct
+	newmsg := make(requestVote)
+	newmsg.Term = s.currentTerm
+	newmsg.CandidateId = 0                               // Current Node ID
+	newmsg.LastLogIndex = len(s.log) - 1                 // Index of last entry in log
+	newmsg.LastLogTerm = s.log[newmsg.lastLogIndex].term // The term of that entry index
 
-    // SEND newmsg REQUESTVOTE RPC BROADCAST
-    zmqMsg := messages.Message{}
-    zmqMsg.Type = "requestVote"
-    zmqMsg.Source = newmsg.client.NodeName
-    zmqMsg.Value = makePayload(newmsg)
-    s.client.Broadcast(zmqMsg)
+	// SEND newmsg REQUESTVOTE RPC BROADCAST
+	zmqMsg := messages.Message{}
+	zmqMsg.Type = "requestVote"
+	zmqMsg.Source = newmsg.client.NodeName
+	zmqMsg.Value = makePayload(newmsg)
+	s.client.Broadcast(zmqMsg)
 }
