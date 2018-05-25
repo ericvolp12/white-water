25b6bacd6f9490e13920557380ea537f951cff7c
Joseph Marques <me@josephmarqu.es>
2018-05-25 13:14:49 -0500

    More control flow changes.

--- raft/raft.go
+++ raft/raft.go
@@ -1,32 +1,35 @@
 package raft
 
 func (s *Sailor) MsgHandler(gets, sets, requestVote, appendEntry *chan messages.Message, timeouts *chan bool) {
-	select {
-	case msg := <-*timeouts:
-		//timeouts message handle
-		//Max
-	default:
+	for {
 		select {
-		case s.state == follower:
-			select {}
-		case s.state == candidate:
-			select {}
-		case s.state == leader:
-			select {}
-		}
-		/*case msg := <-*gets:
-			//get message handle
-			//Joseph
-		case msg := <-*sets:
-			//set message handler
-			//Joseph
-		case msg := <-*requestVote:
-			//requestVote message handle
+		case msg := <-*timeouts:
+			//timeouts message handle
 			//Max
-		case msg := <-*appendEntry:
-			//append message handler
-			//Max*/
-
+		default:
+			select {
+			case s.state == follower:
+				select {
+				//sets - J
+				//gets - J
+				//append - J
+				//requestVote - M
+				}
+			case s.state == candidate:
+				select {
+				//requestVote - M
+				//append -J
+				}
+			case s.state == leader:
+				select {
+				//gets -J
+				//sets -J
+				//append -J
+				//requestVote -M
+				//Propose-value?
+				}
+			}
+		}
 	}
 }
 
