a5dc32dd8e2ea1b13cf89353e3e148f85dc3b9df
Max Demers <mgdemers@uchicago.edu>
2018-05-28 17:49:49 -0500

    adding comments to set.go

--- raft/set.go
+++ raft/set.go
@@ -10,11 +10,13 @@ func (s *Sailor) handle_set(msg messages.Message) {
 	s.leader.queue = append(s.leader.queue, commit_queue{index: 0, commit_count: 0}) //TODO GET INDEX
 }
 
+// Delete an item in the leader's queue of pending commits
 func delete(q []commit_queue, i uint) []commit_queue {
 	q = append(q[:i], q[i+1:]...)
 	return q
 }
 
+// Find the queue entry for MatchIndex and increments the number of commits
 func (s *Sailor) incrementCommit(MatchIndex uint) int {
 	for i, _ := range s.leader.queue {
 		if s.leader.queue[i].index == MatchIndex {
@@ -25,6 +27,8 @@ func (s *Sailor) incrementCommit(MatchIndex uint) int {
 	return -1
 }
 
+// Upon appendReply w/ Sucess, increments the appropriate pending commit count
+// and sends a message to the broker if a majority have now commited
 func (s *Sailor) handle_commit(MatchIndex uint) int {
 	i := s.incrementCommit(MatchIndex)
 	if i == -1 {
