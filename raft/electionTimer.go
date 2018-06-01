caa871021e0238b9c9343989fd5a579dcb5bf16b
Max Demers <mgdemers@uchicago.edu>
2018-05-31 22:49:51 -0500

    a

--- raft/electionTimer.go
+++ raft/electionTimer.go
@@ -11,25 +11,26 @@ func new_time() time.Duration {
 }
 
 func (s *Sailor) Timer(TIMEOUT_SIGNAL chan bool, RESET chan bool) {
+	fmt.Printf("%s Timer Started\n", s.client.NodeName)
 	timer := time.NewTimer(new_time())
 	for {
 		if s.state == leader {
 			select {
 			case <-RESET:
 				timer.Reset(time.Duration(500) * time.Millisecond)
-				fmt.Printf("Timer Reset Leader: %s\n", s.client.NodeName)
+				//fmt.Printf("Timer Reset Leader: %s\n", s.client.NodeName)
 			case <-timer.C:
 				TIMEOUT_SIGNAL <- true
-				fmt.Printf("HEARTBEAT Occured: %s, %s\n", s.client.NodeName, s.state)
+				//fmt.Printf("HEARTBEAT Occured: %s, %s\n", s.client.NodeName, s.state)
 			}
 		} else {
 			select {
 			case <-RESET:
 				timer.Reset(new_time())
-				fmt.Printf("Timer Reset: %s\n", s.client.NodeName)
+				//fmt.Printf("Timer Reset: %s\n", s.client.NodeName)
 			case <-timer.C:
 				TIMEOUT_SIGNAL <- true
-				fmt.Printf("Timeout Occured: %s, %s, %s\n", s.client.NodeName, s.state, s.currentTerm)
+				fmt.Printf("	TIMEOUT OCCURED: %s, %s, %s\n", s.client.NodeName, s.state, s.currentTerm)
 			}
 		}
 	}
