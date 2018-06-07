fb38bb2469a383c5290f133849b4873b124d3e6a
Max Demers <mgdemers@uchicago.edu>
2018-06-07 13:43:11 -0500

    tiny change

--- raft/set.go
+++ raft/set.go
@@ -56,6 +56,7 @@ func (s *Sailor) handle_commit(lowCommit uint, upperCommit uint, state *storage.
 	return nil
 }
 
+// Checks all prepared indices in an appendReply, majority prepared moves up the commit index
 func (s *Sailor) handle_prepare(PrepLower uint, PrepUpper uint) error {
 
 	for i := PrepLower; i <= PrepUpper; i++ {
